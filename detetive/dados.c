0f09876e2d4c485cbb6babbc9a3e2c2b8db5d29b
Daniel Gama <danielgmbr30@gmail.com>
2026-03-10 10:04:21 -0300

    feat: criando banco de dados e inicializando

--- detetive/dados.c
+++ detetive/dados.c
@@ -1,3 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "info.h"
+
+void inicializar_db (LoreDB *db) {
+    db->obras = NULL;
+    db->quant_obras = 0;
+    db->capacidade_obras = 0;
+}
+
--- detetive/main.c
+++ detetive/main.c
@@ -4,6 +4,7 @@
 
 int main()
 {
-
+    LoreDB database; //declara banco de dados
+    inicializar_db(&database); // inicializa (limpa lixo de memoria dentro dele)
     return 0;
 }
